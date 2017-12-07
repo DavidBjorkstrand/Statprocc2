c10e86920dc67bb701696f704c61af6b0ba503e2
David Bjorkstrand <davidbjorkstrand91@gmail.com>
2017-12-07 15:41:09 +0100

    Added more simulations to Task 3

--- Task3.m
+++ Task3.m
@@ -1,7 +1,13 @@
+S_total_total = 0;
 
-S_homogen = homogen_poisson(10);
-S_non_homogen = non_homogen_poisson(10);
-S_total = sort([S_homogen S_non_homogen]);
+for i = 1:1000
+    S_homogen = homogen_poisson(10);
+    S_non_homogen = non_homogen_poisson(10);
+    S_total = sort([S_homogen S_non_homogen]);
+    
+    S_total_total = S_total_total + length(S_total);
+end
+
 
 figure;
 stairs(S_homogen, 1:length(S_homogen));
@@ -12,5 +18,5 @@ stairs(S_non_homogen, 1:length(S_non_homogen));
 figure;
 stairs(S_total, 1:length(S_total));
 
-length(S_total)
+S_total_total = S_total_total / 1000
 
